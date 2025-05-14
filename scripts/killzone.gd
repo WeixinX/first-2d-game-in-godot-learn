c545747e9c019e725eb9d10b9f49d963173433a6
WeixinX <weixin@nuaa.edu.cn>
2025-05-14 16:28:57 +0800

    simple roll

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+var player: CharacterBody2D = null
 
 func _on_body_entered(body: Node2D) -> void:
 	print("you died!")
 	Engine.time_scale = 0.5 # 以半速运行场景
 	body.get_node("CollisionShape2D").queue_free() # 移除 Player 的碰撞节点，使其下坠
+	player = body
+	player.set_state("death")
 	timer.start() # 开始计时
 
 
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1 # 避免重新加载后以慢速运行
+	player.set_state("idle")
 	get_tree().reload_current_scene() # 获取树并重新加载当前场景
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,42 +1,99 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+const ROLL_SPEED = 400
+const ROLL_DURATION = 0.8 # 8 * 10/1000，8 是 roll 的动画帧数，10/1000 是 10 FPS
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+enum PlayerState {IDLE, RUN, JUMP, ROLL, DEATH}
+
+var state = PlayerState.IDLE
+var roll_time = 0 
+var rolling = false
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
-	# 若玩家不在地板上，则添加重力
+	update_state()
+	
+	match state:
+		PlayerState.IDLE:
+			handle_idle()
+		PlayerState.RUN:
+			handle_run()
+		PlayerState.JUMP:
+			handle_jump()
+		PlayerState.ROLL:
+			handle_roll(delta)
+		PlayerState.DEATH:
+			handle_death()
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# 若按下空格键且玩家站在地板上，则跳跃
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# 根据按下的方向键进行左右移动，获取输入方向：-1、0、1
-	var direction := Input.get_axis("move_left", "move_right")
-	
-	# 翻转精灵图
-	if direction > 0:
-		animated_sprite_2d.flip_h = false # 向右移动，不需要翻转精灵图
-	elif direction < 0:
-		animated_sprite_2d.flip_h = true # 向左移动，翻转精灵图
-	
-	# 播放动画
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite_2d.play("idle")
-		else:
-			animated_sprite_2d.play("run")
-	else:
-		animated_sprite_2d.play("jump")
-	
-	# 移动
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
 	move_and_slide()
+
+# TODO
+func update_state():
+	pass
+
+func is_jump():
+	return Input.is_action_pressed("jump") and is_on_floor()	
+
+func handle_idle():
+	animated_sprite.play("idle")
+	velocity.x = 0
+	
+	if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
+		state = PlayerState.RUN
+	elif is_jump():
+		state = PlayerState.JUMP
+	elif Input.is_action_pressed("roll"):
+		state = PlayerState.ROLL
+
+func handle_run():
+	animated_sprite.play("run")
+	
+	if Input.is_action_pressed("move_left"):
+		animated_sprite.flip_h = true
+		velocity.x = -SPEED
+	elif Input.is_action_pressed("move_right"):
+		animated_sprite.flip_h = false
+		velocity.x = SPEED
+	else:
+		state = PlayerState.IDLE
+		
+	if is_jump():
+		state = PlayerState.JUMP
+	elif Input.is_action_pressed("roll"):
+		state = PlayerState.ROLL
+	
+func handle_jump():
+	animated_sprite.play("jump")
+	velocity.y = JUMP_VELOCITY
+	state = PlayerState.IDLE # 否则会一直飞
+	
+
+# 简单实现了下 roll
+func handle_roll(delta: float):
+	if !rolling:
+		animated_sprite.play("roll")
+		rolling = true
+	else:
+		roll_time += delta
+		if roll_time >= ROLL_DURATION:
+			rolling = false
+			state = PlayerState.IDLE
+			roll_time = 0
+			
+func handle_death():
+	animated_sprite.play("death")
+	
+func set_state(s: String):
+	match s:
+		"idle":
+			state = PlayerState.IDLE
+		"death":
+			state = PlayerState.DEATH
+			
